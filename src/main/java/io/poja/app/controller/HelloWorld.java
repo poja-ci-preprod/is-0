b1773ee16928984985f11df05270f9939557512a
yumii.saiko <hei.hajatiana@gmail.com>
2025-11-06 18:53:13 +0300

    chore: init springboot maven

---
+++ src/main/java/io/poja/app/controller/HelloWorld.java
@@ -0,0 +1,12 @@
+package io.poja.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+  @GetMapping("/hello-world")
+  public String helloWorld() {
+    return "Hello, world";
+  }
+}
---
+++ src/test/java/io/poja/app/SpringMvnApplicationTests.java
@@ -0,0 +1,13 @@
+package io.poja.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvnApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
