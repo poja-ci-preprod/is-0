b12bd17ab9052375e259ee1f5bbc910c911648f9
yumii.saiko <hei.hajatiana@gmail.com>
2025-11-07 11:49:23 +0300

    chore: set correct groupId

--- src/main/java/io/poja/importa/PojaApplication.java
+++ src/main/java/io/poja/importa/PojaApplication.java
@@ -1,4 +1,4 @@
-package io.poja.app;
+package io.poja.importa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/io/poja/importa/SpringMvnApplicationTests.java
+++ src/test/java/io/poja/importa/SpringMvnApplicationTests.java
@@ -1,4 +1,4 @@
-package io.poja.app;
+package io.poja.importa;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
